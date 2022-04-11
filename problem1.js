8b04fe4d91d554f24f42d07d4a7ddb0f2995e794
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-04-11 13:05:55 +0530

    remove the extra promise binding

--- problem1.js
+++ problem1.js
@@ -1,41 +1,35 @@
 // Problem 1:
-    
+
 // Using promises and the fs module's asynchronous functions, do the following:
 //     1. Create a directory of random JSON files
 //     2. Delete those files simultaneously 
 
 
-const fs= require('fs');
+const fs = require('fs');
 const path = require('path');
 
 
-function createDir(Path) {
-    return new Promise((res, rej) => {
-        fs.promises.mkdir(Path)
-        .then(() => res("directory created!"))
-        .catch((err) => rej(err))
-    })
+async function createDir(Path) {
+    return fs.promises.mkdir(Path)
+        .then(() => console.log("directory created"))
+        .catch((err) => console.log(err))
 }
 
 
-function createFile(filepath, data){
-    return new Promise((res, rej) => {
-        fs.promises.writeFile(filepath, data, 'utf-8')
-        .then(() => res(filepath) )       
-        .catch((err) => rej(err))
-    })
+async function createFile(filepath, data) {
+    return fs.promises.writeFile(filepath, data, 'utf-8')
+        .then(() => console.log("file created: ",filepath))
+        .catch(() => console.log(err))
 }
 
 
-function deleteFile(filepath){
-    return new Promise((res, rej) => {
-        fs.promises.unlink(filepath)
-        .then(() => res(filepath))
-        .catch((err) => rej(err))
-    })
+function deleteFile(filepath) {
+    fs.promises.unlink(filepath)
+        .then(() => console.log("file deleted: ",filepath))
+        .catch(() => console.log(err))
 }
 
 
 
 
-module.exports = {createDir, createFile, deleteFile}
+module.exports = { createDir, createFile, deleteFile }
