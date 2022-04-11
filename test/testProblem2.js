f126e1b63a509e0c2528ad5ad9408f41e308e31e
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-04-11 13:09:05 +0530

    .then chaining

--- test/testProblem2.js
+++ test/testProblem2.js
@@ -2,18 +2,18 @@ const test = require('../problem2.js')
 const path = require('path')
 
 test.fileRead('lipsum.txt')
-    .then((res) => {console.log("file Readed")
-        test.convertToUpperCase('lipsum.txt', 'fileOfUpperData.txt')
-            .then((res) => {console.log(res)
-                test.splitIntoSentences('fileOfUpperData.txt', 'fileOfSentences.txt')
-                    .then((res) => {console.log(res)
-                        test.sortData('fileOfSentences.txt', 'fileOfSortedData.txt')
-                            .then((res) => {console.log(res)
-                                test.deleteFile('test/filenames.txt')
-                                .then((res) => {console.log(res)})
-                            })
-                    })
-            })
-    }).catch((rej) => console.log(rej))
-
-
+    .then((res) => {
+        console.log("file Readed: ", res)
+        return test.convertToUpperCase('lipsum.txt', 'fileOfUpperData.txt');
+    })
+    .then(() => {
+        return test.splitIntoSentences('fileOfUpperData.txt', 'fileOfSentences.txt')
+    })                    
+    .then(() => {
+        return test.sortData('fileOfSentences.txt', 'fileOfSortedData.txt')
+    })
+    .then(() => {
+        return test.deleteFile('test/filenames.txt')
+    })
+    .then((res) => {console.log(res)})
+    .catch((err) => console.log(err))
